e2cca8e46247c4b6166c70a4ef6a1eee9f1fa682
Elizabeth Mc Nally <eli_mcnally@hotmail.com>
2025-08-26 00:10:14 +0200

    fix app.js

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,7 +12,7 @@ export default function App() {
 
   return (
     <div style={{ fontFamily: 'system-ui, -apple-system, Segoe UI, Roboto, Ubuntu, Cantarell, Noto Sans, Helvetica, Arial, Apple Color Emoji, Segoe UI Emoji' }}>
-      <h1>🚀 Proyecto Base (React + Express)</h1>
+      <h1>🚀 Proyecto Base Template(React + Express)</h1>
       <p>Backend /health: <strong>{status}</strong></p>
       <p style={{opacity:0.7}}>Frontend: Vite (5173) — Backend: Express (4000)</p>
     </div>
